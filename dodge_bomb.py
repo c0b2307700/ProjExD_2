040a175bafc9afcaabd6418dffc165a58f42f949
c0b23077000 <c0b2307700@edu.teu.ac.jp>
2024-10-01 14:30:35 +0900

    練習2: 爆弾の生成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bd_img = pg.Surface((20, 20))  # からのsurface
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    # bb_rct.centerX = random.randint(0, WIDTH)
+    # bb_rct.centerY = random.randint(0, HEIGHT)  # このように表記できる
+    vx, vy = +5, +5  # 爆弾の移動の変化量
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -43,11 +53,12 @@ def main():
                 sum_mv[1] += tpl[1]  # 縦方向
 
         kk_rct.move_ip(sum_mv)
+        bd_rct.move_ip((vx, vy))
         screen.blit(kk_img, kk_rct)
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
